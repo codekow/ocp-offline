697903e86a909b704b1a0e8843efb3740d28a902
Cory Latschkowski <clatschk@redhat.com>
2026-03-04 19:36:11 -0600

    update: container

---
+++ container/download_tools.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -xeuo pipefail
+
+BIN_PATH=scratch/usr/local/bin
+BASH_COMP=scratch/etc/bash_completion.d
+
+[ -d "${BIN_PATH}" ] || mkdir -p "${BIN_PATH}"
+[ -d "${BASH_COMP}" ] || mkdir -p "${BASH_COMP}"
+
+PATH=${BIN_PATH}:${PATH}
+
+SCRIPT_URL=https://raw.githubusercontent.com/redhat-na-ssa/demo-ai-gitops-catalog/v0.20/scripts/library
+
+curl -sLO "${SCRIPT_URL}/bin.sh"
+
+# shellcheck disable=SC1091
+. bin.sh
+
+# cleanup
+rm -rf scratch .oc-mirror.log tools-x86_64.tgz
+
+bin_check oc
+bin_check oc-mirror
+bin_check openshift-install
+
+chmod 755 "${BIN_PATH}"/*
+
+rm "${BIN_PATH}"/LICENSE
+tar -czf tools-x86_64.tgz -C scratch . --owner=0 --group=0
