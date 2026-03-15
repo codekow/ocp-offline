55095786723eebd9c5fa01a575f94f8771bf2021
Cory Latschkowski <clatschk@redhat.com>
2026-03-15 16:29:34 -0600

    refactor / rewrite (#1)
    * add: all the things
    
    * update: all the things

--- docs/NOTES.md
+++ docs/NOTES.md
@@ -1,1 +1,102 @@
-# General Notes
+# OCP Disconnected install notes
+
+## General Info
+
+There are multiple options for installing OCP in a disconnected environment:
+
+- Single ISO (no external registry)
+- Agent based install
+
+Additional options
+
+- Installer Provisioned Install (IPI)- 3 masters + 1 bastion
+- User Provisioned Install (UPI)
+
+## Issues
+
+- OCP `4.20.16` is not currently available on the `stable-4.20` channel. This version in the `isc.yaml` caused issues with the `oc-mirror` command
+
+## Links
+
+- [Installing on bare metal](https://docs.redhat.com/en/documentation/openshift_container_platform/4.20/html-single/installing_on_bare_metal)
+- [Installing with Agent Based Installer](https://docs.redhat.com/en/documentation/openshift_container_platform/4.20/html-single/installing_an_on-premise_cluster_with_the_agent-based_installer/index#installing-ocp-agent-inputs_installing-with-agent-based-installer)
+- [Installing a mirrored registry in a disconnected environment - Prereqs](https://docs.redhat.com/en/documentation/openshift_container_platform/4.20/html-single/disconnected_environments/index#prerequisites_installing-mirroring-creating-registry)
+- [Installing a mirrored registry in a disconnected environment](https://docs.redhat.com/en/documentation/openshift_container_platform/4.20/html-single/disconnected_environments/index#installing-mirroring-creating-registry)
+- [oc-mirror in a partially disconnected env](https://docs.redhat.com/en/documentation/openshift_container_platform/4.20/html-single/disconnected_environments/index#oc-mirror-workflows-partially-disconnected-v2_about-installing-oc-mirror-v2)
+- [Installing a cluster without an external registry - Single ISO Download](https://docs.redhat.com/en/documentation/openshift_container_platform/4.20/html-single/installing_an_on-premise_cluster_with_the_agent-based_installer/index#installing-ove)
+  - [x] I’m installing on a disconnected/air-gapped/secured environment
+- [Agent Based Installer - Home Lab Notes](https://github.com/mariocr73/OCP-ABI-BAREMETAL)
+
+## Commands
+
+Install `nmstatectl` on your bastion host - this is required to verify your network config in `agent-config.yaml`
+
+```sh
+# nmstatectl is required for config validation
+sudo dnf install /usr/bin/nmstatectl -y
+```
+
+Create a `pull-secret.txt`
+
+```sh
+# !! MANUAL !!
+# create pull-secret
+# https://console.redhat.com/openshift/downloads#tool-pull-secret
+# vi pull-secret.txt
+```
+
+```sh
+git clone https://github.com/codekow/ocp-offline
+cd ocp-offline
+
+# make new folder for all the artifacts
+mkdir ocp-install
+cd ocp-install
+
+# load functions
+. ../scripts/functions.sh
+
+download_files
+```
+
+```sh
+# !! IMPORTANT !! - these versions should match
+# openshift-install needs to match the version(s) in the isc.yaml (oc-mirror)
+
+oc version
+openshift-install version
+
+# setup autocomplete
+. <(oc completion bash)
+. <(openshift-install completion bash)
+. <(oc-mirror --v2 completion bash)
+```
+
+```sh
+# copy the isc.yaml into current dir
+cp ../dump/agent/isc-ocp*.yaml .
+
+# these commands are used to create tar(s) and load the tar(s) into the disconnected mirror
+# oc_mirror_src2files
+# oc_mirror_files2mirror
+
+# install mirror-registry
+# this folder will have the extracted mirror-registry files
+cd quay
+mirror_registry_install /srv/registry
+cd ..
+
+# directly mirror what is online to a disconnected registry
+oc_mirror_src2mirror
+```
+
+```sh
+# login to mirror registry
+podman login $(hostname):8443
+```
+
+Create an iso to add worker nodes to an existing cluster
+
+```sh
+oc adm node-image create
+```
---
+++ scripts/functions.sh
@@ -0,0 +1,198 @@
+#!/bin/bash
+
+confirm_cmd(){
+  echo "Run the following?
+  "
+  echo "$@
+  "
+  
+  [ "${CONFIRM}" = "always" ] || read -p \
+    "[Y]es / [n]o / [a]lways " CONFIRM
+
+  case ${CONFIRM} in
+    [yY]*)
+      $@
+    ;;
+    [aA]*)
+      CONFIRM=always
+      $@
+    ;;
+    *)
+      return 0
+    ;;
+  esac
+}
+
+check_prereqs(){
+  which nmstatectl || sudo dnf install /usr/bin/nmstatectl -y
+}
+
+check_cli(){
+  # verify cli in path
+  which oc && oc version 
+  which openshift-install && openshift-install version
+  which oc-mirror
+}
+
+download_files(){
+  SCRATCH=${1:-${PWD}}
+  OCP_VER=${2:-4.20.15}
+
+  [ -e ~/bin ] || mkdir -p ~/bin
+
+  [ -e "${SCRATCH}" ] || mkdir -p "${SCRATCH}"
+
+  cd ${SCRATCH}
+  [ -e tmp ] || mkdir tmp
+
+  # create pull-secret
+  # https://console.redhat.com/openshift/downloads#tool-pull-secret
+  # !! MANUAL !!
+  # vi pull-secret.txt
+
+  # ocp mirror
+  # https://mirror.openshift.com/pub/openshift-v4/amd64/clients/ocp/
+
+  # get oc
+  wget -c -nc https://mirror.openshift.com/pub/openshift-v4/amd64/clients/ocp/${OCP_VER}/openshift-client-linux-${OCP_VER}.tar.gz
+  tar vzxf openshift-client-*.tar.gz
+  [ -e README.md ] && (cat README.md; rm README.md)
+  mv oc kubectl ~/bin
+
+  # get openshift-install
+  wget -c -nc https://mirror.openshift.com/pub/openshift-v4/clients/ocp/${OCP_VER}/openshift-install-linux.tar.gz
+  tar vzxf openshift-install-*.tar.gz
+  [ -e README.md ] && (cat README.md; rm README.md)
+  mv openshift-install ~/bin
+
+  # get oc-mirror
+  wget -c -nc https://mirror.openshift.com/pub/openshift-v4/amd64/clients/ocp/${OCP_VER}/oc-mirror.tar.gz
+  tar vzxf oc-mirror*.tar.gz
+  [ -e README.md ] && (cat README.md; rm README.md)
+  chmod +x oc-mirror
+  mv oc-mirror ~/bin
+
+  # get mirror-registry
+  wget -c -nc https://mirror.openshift.com/pub/cgw/mirror-registry/latest/mirror-registry-amd64.tar.gz
+
+  # unpack mirror-registry files in dir
+  [ -e quay ] || mkdir quay
+  cd quay
+
+  tar vzxf ../mirror-registry*.tar.gz
+  cd ..
+}
+
+oc_mirror_src2files(){
+  SCRATCH=${SCRATCH:-${PWD}}
+
+  TMPDIR=${SCRATCH} \
+  oc-mirror --v2 \
+    -c ${SCRATCH}/isc.yaml \
+    --cache-dir ${SCRATCH}/cache \
+    --authfile ${SCRATCH}/pull-secret.txt \
+    --image-timeout 60m \
+      file://${SCRATCH}/files
+}
+
+oc_mirror_files2mirror(){
+  SCRATCH=${SCRATCH:-${PWD}}
+  REG_HOST=${REG_HOST:-$(hostname):8443}
+
+  pull_secret_merge_with_mirror
+
+  TMPDIR=${SCRATCH} \
+  oc-mirror --v2 \
+    -c ${SCRATCH}/isc.yaml \
+    --dest-tls-verify=false \
+    --authfile ${SCRATCH}/pull-combo.json \
+    --image-timeout 60m \
+    --from file://${SCRATCH}/files \
+      docker://${REG_HOST}/redhat
+}
+
+oc_mirror_src2mirror(){
+  SCRATCH=${SCRATCH:-${PWD}}
+  REG_HOST=${REG_HOST:-$(hostname):8443}
+
+  pull_secret_merge_with_mirror
+
+  TMPDIR=${SCRATCH} \
+  oc-mirror --v2 \
+    -c ${SCRATCH}/isc.yaml \
+    --dest-tls-verify=false \
+    --workspace file://${SCRATCH}/workspace \
+    --authfile ${SCRATCH}/pull-combo.json \
+    --image-timeout 60m \
+      docker://${REG_HOST}/redhat
+}
+
+mirror_registry_install(){
+
+  [ -x mirror-registry ] || return 0
+
+  REG_PATH=${1:-${PWD}}
+  REG_USER=${2:-init}
+  REG_PASS=${3:-alongpassword}
+
+  mkdir -p ${REG_PATH}/{config,data,db}
+
+  ./mirror-registry install \
+    --initUser "${REG_USER}" \
+    --initPassword "${REG_PASS}" \
+    --sqliteStorage ${REG_PATH}/db \
+    --quayRoot ${REG_PATH}/config \
+    --quayStorage ${REG_PATH}/data
+
+  # update CA trust
+  cat ${REG_PATH}/config/quay-rootCA/rootCA.pem | sudo tee /etc/pki/ca-trust/source/anchors/quay.pem
+  sudo update-ca-trust extract
+
+  # open firewall (optional)
+  sudo firewall-cmd --add-port=8443/tcp --permanent
+  sudo firewall-cmd --reload
+
+  podman login ${REG_HOST}:8443 -u "${REG_USER}" -p "${REG_PASS}"
+}
+
+mirror_registry_uninstall(){
+  [ -x mirror-registry ] || return 0
+
+  sudo rm /etc/pki/ca-trust/source/anchors/quay.pem
+  sudo update-ca-trust extract
+
+  sudo firewall-cmd --remove-port=8443/tcp --permanent
+  sudo firewall-cmd --reload
+
+  ./mirror-registry uninstall
+}
+
+pull_secret_merge_with_mirror(){
+  [ -e pull-secret.txt ] || return 0
+  [ -e ${XDG_RUNTIME_DIR}/containers/auth.json ] || return 0
+  which jq || return 0
+
+  jq -s '.[0] * .[1]' pull-secret.txt ${XDG_RUNTIME_DIR}/containers/auth.json > pull-combo.json
+
+  [ -e auth.json ] || mv ${XDG_RUNTIME_DIR}/containers/auth.json auth.json
+  cp pull-combo.json ${XDG_RUNTIME_DIR}/containers/auth.json
+}
+
+extract_baremetal_install(){
+  # https://access.redhat.com/solutions/7062500
+
+  oc adm release extract \
+    -a pull-combo.json \
+    --command=openshift-baremetal-install \
+    "${REG_HOST}/redhat/openshift/release-images:${OCP_VER:-4.20.15}-x86_64"
+}
+
+extract_iso(){
+  oc image extract \
+    -a pull-combo.json \
+    --path=/coreos/coreos-x86_64.iso:${HOME}/.cache/agent/image_cache \
+    --filter-by-os=linux/amd64 \
+    --confirm \
+    ${REG_HOST}/redhat/openshift/release:${OCP_VER:-4.20.15}-x86_64-rhel-coreos
+    # quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:f93e0978db7e4a065b8722d023bcf9d7e0dbe3cd1e78d83a190c168f205147fe
+}
